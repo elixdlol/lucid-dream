cd5add12e79622943690f032b1d3c511c073654a
RonenIsGreat <40147121+RonenIsGreat@users.noreply.github.com>
2019-11-03 14:43:47 +0200

    Added tracks data receive by rabbit

---
+++ TrackBeamParser/TrackBeamParser/TrackData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrackBeamParser
+{
+    public class TrackData
+    {
+        public int TrackNumber;
+        public int TrackAngle;
+    }
+}
