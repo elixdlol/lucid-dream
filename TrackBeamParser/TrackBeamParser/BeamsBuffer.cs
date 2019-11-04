dbe1c0e931bd28797d56c199b6c91fd620ed0e8b
RonenIsGreat <40147121+RonenIsGreat@users.noreply.github.com>
2019-11-04 10:14:59 +0200

    Prepare BeamsBuffer class for Rageb :)

--- TrackBeamParser/TrackBeamParser/BeamsBuffer.cs
+++ TrackBeamParser/TrackBeamParser/BeamsBuffer.cs
@@ -6,7 +6,15 @@ namespace TrackBeamParser
 {
     public static class BeamsBuffer
     {
+        static byte[][] Beams = new byte[beamsNumber][];
+        const int beamsNumber= 192;
         public static Object Locker = new Object();
+
+        public static void writeBeams(byte[][] beams)
+        {
+            // append to end of every beam array
+        }
+
         public static byte[][] getBeamsAndFlush()
         {
             lock (Locker)
@@ -19,7 +27,7 @@ namespace TrackBeamParser
 
         private static void cleanBeams()
         {
-            throw new NotImplementedException();
+            Beams = new byte[beamsNumber][];
         }
     }
 }
