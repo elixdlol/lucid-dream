0d86011d3027c84f761841f9b787f4e9bf426d19
yakir karandian <yakir1114@gmail.com>
2019-11-04 13:53:00 +0000

    Adding BDT-CAS microservice

---
+++ lucidDBManager/LucidDream_BDT_Microservice/src/RabbitMQSender.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LucidDream_ManagedContractManagerSystem_VS2010x64.src
+{
+    class RabbitMQSender
+    {
+    }
+}
