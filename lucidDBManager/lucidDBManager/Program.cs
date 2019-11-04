ec53d019ca162de79d3a239fb3e03e10d2538f8d
ferasghanim <53860528+ferasghanim@users.noreply.github.com>
2019-11-04 16:10:49 +0200

    create mongoDB server

--- lucidDBManager/lucidDBManager/Program.cs
+++ lucidDBManager/lucidDBManager/Program.cs
@@ -6,14 +6,14 @@ namespace lucidDBManager
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-           // var receiver = new RabbitMQReciever();
-            var sender = new RabbitMQSender();
+        //static void Main(string[] args)
+        //{
+        //   // var receiver = new RabbitMQReciever();
+        //    var sender = new RabbitMQSender();
 
-            TrackData data = new TrackData() { trackID = 1, relativeBearing = (float)0.05 };
+        //    TrackData data = new TrackData() { trackID = 1, relativeBearing = (float)0.05 };
 
-            sender.sendTrackData(data);
-        }
+        //    sender.sendTrackData(data);
+        //}
     }
 }
