2e2ecc90ea1b976f2ea175c278ef17f3abd2a2fe
ferasghanim <53860528+ferasghanim@users.noreply.github.com>
2019-11-05 17:18:44 +0200

    create the OwnBoatData struct

--- lucidDBManager/lucidDBManager/Data/OwnBoatData.cs
+++ lucidDBManager/lucidDBManager/Data/OwnBoatData.cs
@@ -1,23 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using static lucidDBManager.Data.BasicData;
 
 namespace lucidDBManager.Data
 {
     public class OwnBoatData
     {
-        TimeType systemTime;
-        //timezone
-        float heading;
-        // heading rate
-        float roll;
-        // roll rate
-        float pitch;
-        // pitch rate
-        float heave;
-        // heave rate
-        float courseoverground;
+        public Timezone timeZone = new Timezone();
+        public Heading heading = new Heading();
+        public Roll roll = new Roll();
+        public Pitch pitch = new Pitch();
+        public Heave heave = new Heave();
+        public CourseOverGround course_overe_ground = new CourseOverGround();
     }
 
-  
+    public class OwnBoatHeading
+    {
+        public ValidLongRealType data = new ValidLongRealType();
+        public bool is_current;
+        public long sensor;
+        public ValidTimeStampType time = new ValidTimeStampType();
+    }
+    public class OwnBoatRoll
+    {
+        public ValidLongRealType data = new ValidLongRealType();
+        public bool is_current;
+        public long sensor;
+        public ValidTimeStampType time = new ValidTimeStampType();
+    }
+    public class OwnBoatPitch
+    {
+        public ValidLongRealType data = new ValidLongRealType();
+        public bool is_current;
+        public long sensor;
+        public ValidTimeStampType time = new ValidTimeStampType();
+    }
+    public class OwnBoatHeave
+    {
+        public ValidLongRealType data = new ValidLongRealType();
+        public bool is_current;
+        public long sensor;
+        public ValidTimeStampType time = new ValidTimeStampType();
+    }
+    public class OwnBoatCourseOverGround
+    {
+        public ValidLongRealType data = new ValidLongRealType();
+        public bool is_current;
+        public long sensor;
+        public ValidTimeStampType time = new ValidTimeStampType();
+    }
+
+    public class ValidTimeStampType
+    {
+        public bool valid;
+        public TimeType value = new TimeType();
+    }
+
+    public class ValidLongRealType
+    {
+        public bool valid;
+        public double value;
+    }
 }
